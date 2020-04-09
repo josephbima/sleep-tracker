98123fef3d3ab7246bb54c42822b3bb1c9cc0e88
Joseph Bimansyah Pasha Jenie <bima.jenie@gmail.com>
2020-04-10 03:00:25 +0700

    changed default code

---
+++ collect-labelled-data.py
@@ -0,0 +1,120 @@
+import socket
+import sys
+import json
+import numpy as np
+
+user_id = "team-jj"
+
+# TODO: Set label_name to the activity you are doing
+label_name = "vigil"
+# Vigil, Sleeping
+#
+ 
+"""
+TODO: Set label_index to the activity index, ranging from 0 to (num_of_activities-1) 
+Your classifier will return the index of the activity and you will get the label name
+from this index in activity-recognition.py
+"""
+
+label_index = 0
+
+#################   Begin Server Connection Code  ####################
+
+def authenticate(sock):
+    """
+    Authenticates the user by performing a handshake with the data collection server.
+    
+    If it fails, it will raise an appropriate exception.
+    """
+    msg_request_id = "ID"
+    msg_authenticate = "ID,{}\n"
+    msg_acknowledge_id = "ACK"
+    
+    message = sock.recv(256).strip().decode('ascii')
+    if (message == msg_request_id):
+        print("Received authentication request from the server. Sending authentication credentials...")
+    else:
+        print(type(message))
+        print("Authentication failed!")
+        raise Exception("Expected message {} from server, received {}".format(msg_request_id, message))
+    sock.send(msg_authenticate.format(user_id).encode('utf-8'))
+
+    try:
+        message = sock.recv(256).strip().decode('ascii')
+    except:
+        print("Authentication failed!")
+        raise Exception("Wait timed out. Failed to receive authentication response from server.")
+        
+    if (message.startswith(msg_acknowledge_id)):
+        ack_id = message.split(",")[1]
+    else:
+        print("Authentication failed!")
+        raise Exception("Expected message with prefix '{}' from server, received {}".format(msg_acknowledge_id, message))
+    
+    if (ack_id == user_id):
+        print("Authentication successful.")
+        sys.stdout.flush()
+    else:
+        print("Authentication failed!")
+        raise Exception("Authentication failed : Expected user ID '{}' from server, received '{}'".format(user_id, ack_id))
+        
+try:
+    # This socket is used to receive data from the data collection server
+    receive_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    receive_socket.connect(("none.cs.umass.edu", 8888))
+
+    # ensures that after 1 second, a keyboard interrupt will close
+    receive_socket.settimeout(1.0)
+    
+    print("Authenticating user for receiving data...")
+    sys.stdout.flush()
+    authenticate(receive_socket)
+    
+    print("Successfully connected to the server! Waiting for incoming data...")
+    sys.stdout.flush()
+        
+    previous_json = ''
+    labelled_data = []
+
+    while True:
+        try:
+            message = receive_socket.recv(1024).strip().decode('ascii')
+            json_strings = message.split("\n")
+            json_strings[0] = previous_json + json_strings[0]
+            for json_string in json_strings:
+                try:
+                    data = json.loads(json_string)
+                except:
+                    previous_json = json_string
+                    continue
+                previous_json = '' # reset if all were successful
+                sensor_type = data['sensor_type']
+                if (sensor_type == u"SENSOR_ACCEL"):
+                    t=data['data']['t']
+                    x=data['data']['x']
+                    y=data['data']['y']
+                    z=data['data']['z']
+                    labelled_data.append([t, x, y, z, label_index])
+                    
+                    print("Received Accelerometer data with label " + str(label_index))
+                    
+            sys.stdout.flush()
+        except KeyboardInterrupt: 
+            # occurs when the user presses Ctrl-C
+            labelled_data = np.asarray(labelled_data)
+            np.savetxt(label_name+"-data.csv", labelled_data, delimiter=",", fmt="%s")
+            print("User Interrupt. Saving and quitting...")
+            break
+        
+        except Exception as e:
+            # ignore exceptions, such as parsing the json
+            # if a connection timeout occurs, also ignore and try again. Use Ctrl-C to stop
+            # but make sure the error is displayed so we know what's going on
+            if (str(e) != "timed out"):  # ignore timeout exceptions completely       
+                print(e)
+            pass
+
+except KeyboardInterrupt: 
+    # occurs when the user presses Ctrl-C
+    print("User Interrupt. Quitting...")
+    quit()
