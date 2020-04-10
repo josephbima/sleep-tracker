7c60682d152c681f503acf113bfdf2564918ab91
Joseph Bimansyah Pasha Jenie <bima.jenie@gmail.com>
2020-04-10 11:13:44 +0700

    Plotter fixed

--- plotter.py
+++ plotter.py
@@ -1,19 +1,27 @@
 import matplotlib.pyplot as plt
 import csv
 
+t = []
 x = []
 y = []
+z = []
 
-with open('sleep-joseph-data.csv','r') as csvfile:
+with open('sleep-data.csv','r') as csvfile:
     plots = csv.reader(csvfile, delimiter=',')
     for row in plots:
-        x.append(str(row[0]))
-        y.append(row[1])
+        t.append(row[0])
+        x.append((row[1]))
+        y.append(row[2])
+        z.append(row[3])
+        print(row)
+        
 
+plt.plot(t,x, label='X axis')
+plt.plot(t,y, label='Y axis')
+plt.plot(t,z, label='Z axis')
 
-plt.plot(x,y, label='Loaded from file!')
 plt.xlabel('Time')
-plt.ylabel('y')
-plt.title('Interesting Graph\nCheck it out')
+plt.ylabel('Axis')
+plt.title('Axis/Time Graph')
 plt.legend()
 plt.show()
